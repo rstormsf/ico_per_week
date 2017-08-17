f0cf2a87b48df1c2ef9ca0e4ff854f4196ce6dae
Roman Storm <rstormsf@gmail.com>
2017-08-17 02:44:59 -0700

    whitelisted crowdsale

--- contracts/BlockchainLabsCrowdsale.sol
+++ contracts/BlockchainLabsCrowdsale.sol
@@ -2,14 +2,33 @@ pragma solidity ^0.4.15;
 
 import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 import "zeppelin-solidity/contracts/token/MintableToken.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 import "./BlockchainLabsToken.sol";
 
-contract BlockchainLabsCrowdsale is Crowdsale {
+contract BlockchainLabsCrowdsale is Crowdsale, Ownable {
+    mapping(address => bool) public addresses;
+    bool whitelistInitialized = false;
     function BlockchainLabsCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) 
         Crowdsale(_startTime, _endTime, _rate, _wallet)
     {
+    }
 
+    function whitelistAddresses(address[] _addresses) onlyOwner {
+        require(!whitelistInitialized);
+        for (uint256 i = 0; i < _addresses.length; i++) {
+            addresses[_addresses[i]] = true;
+        }
+        whitelistInitialized = true;
+    }
+    
+    function buyTokens(address beneficiary) payable {
+        require(addresses[beneficiary] == true);
+        super.buyTokens(beneficiary);
+    }
+
+    function changeWhitelistAddress(address _address, bool state) onlyOwner {
+        addresses[_address] = state;
     }
 
     function createTokenContract() internal returns (MintableToken) {
